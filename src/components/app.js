970e1d7d6a5eebedf3dbfc89458c662d48a936ef
jessebettridge <jessebett92@gmail.com>
2019-07-10 20:01:19 -0600

    Added functionality to nav components

--- src/components/app.js
+++ src/components/app.js
@@ -1,18 +1,42 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import moment from "moment";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 import PortfolioContainer from './portfolio/portfolio-container';
 import NavigationContainer from './navigation/navigation-container';
+
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
 
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <NavigationContainer />
-        <h1>Jesse Bettridge Portfolio</h1>
-        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
-        <PortfolioContainer />
+
+        <Router>
+          <div>
+            <h1>Jesse Bettridge Portfolio</h1>
+            <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
+
+            <NavigationContainer />
+
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route path="/about-me" component={About}/>
+              <Route path="/contact" component={Contact}/>
+              <Route path="/blog" component={Blog}/>
+
+            </Switch>
+          </div>
+        </Router>
+
       </div>
     )
   }
---
+++ src/components/pages/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PortfolioContainer from "../portfolio/portfolio-container";
+
+export default function() {
+  return (
+    <div>
+      <h2>Homepage</h2>
+      <PortfolioContainer />
+    </div>
+  );
+}
