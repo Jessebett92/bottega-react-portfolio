79cee53ebaa94637098e8f3a2898a98da070c30d
jessebettridge <jessebett92@gmail.com>
2019-07-16 23:53:09 -0600

    added portfolio management component

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -7,8 +7,8 @@ const NavigationComponent = props => {
   const dynamicLink = (route, linkText) => {
     return (
       <div className="nav-link-wrapper">
-        <NavLink to="/blog" activeClassName="nav-link-active">
-          Blog
+        <NavLink to={route} activeClassName="nav-link-active">
+          {linkText}
         </NavLink>
       </div>
     );
@@ -37,19 +37,27 @@ const NavigationComponent = props => {
             Home
           </NavLink>
         </div>
+
         <div className="nav-link-wrapper">
           <NavLink to="/about-me" activeClassName="nav-link-active">
             About
           </NavLink>
         </div>
+
         <div className="nav-link-wrapper">
           <NavLink to="/contact" activeClassName="nav-link-active">
             Contact
           </NavLink>
         </div>
 
+        <div className="nav-link-wrapper">
+          <NavLink to="/blog" activeClassName="nav-link-active">
+            Blog
+          </NavLink>
+        </div>
+
         {props.loggedInStatus === "LOGGED_IN"
-          ? dynamicLink("/blog", "Blog")
+          ? dynamicLink("/portfolio-manager", "Portfolio Manager")
           : null}
       </div>
 
---
+++ src/components/pages/portfolio-manager.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list";
+
+export default class PortfolioManager extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      portfolioItems: []
+    };
+  }
+
+  getPortfolioItems() {
+    axios
+      .get("https://jessebettridge.devcamp.space/portfolio/portfolio_items", {
+        withCredentials: true
+      })
+      .then(response => {
+        this.setState({
+          portfolioItems: [...response.data.portfolio_items]
+        });
+      })
+      .catch(error => {
+        console.log("Error in Getting Portfolio Items", error);
+      });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
+  render() {
+    return (
+      <div className="portfolio-manager-wrapper">
+        <div className="left-column">
+          <h1>Portfolio form...</h1>
+        </div>
+
+        <div className="right-column">
+          <PortfolioSidebarList data={this.state.portfolioItems} />
+        </div>
+      </div>
+    );
+  }
+}
