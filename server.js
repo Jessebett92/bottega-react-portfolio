abb0ff25eef656b39b5a2423030f93fbd9e0658d
jessebettridge <jessebett92@gmail.com>
2019-07-26 15:49:26 -0600

    Changed env port

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const port = process.env.PORT || 8080;
+const express = require("express");
+const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static(__dirname + '/dist/'));
-app.get(/.*/, function (req, res) {
-  res.sendFile(__dirname + '/dist/index.html');
-})
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, function(req, res) {
+  res.sendFile(__dirname + "/dist/index.html");
+});
 app.listen(port);
 
 console.log("server started");
